85a54f9911c49851402e05ff90ba2c09a359047f
AakashR11111001111 <aakash04721202019@msi-ggsip.org>
2024-11-18 16:19:04 +0530

    bugs fixed

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 var mainContainer = document.querySelector("#main-container");
 var cursor = document.querySelector("#cursor");
 var fotuContainer = document.querySelector(".img-container");
+var pCount = null;
 
-window.addEventListener("load", async ()=>{
+
+
+async function showPics(){
+
     let fetfotu = await fetch('https://api.unsplash.com/photos/random/?client_id=nOZ8-oQTkpz_oTsDbhOoNSsuzj0drJWYzfM2Ej20DfU&count=10');
     let jfotu = await fetfotu.json();
     jfotu.forEach(fotus =>{
@@ -15,32 +19,18 @@ window.addEventListener("load", async ()=>{
         div.innerHTML = code;
         fotuContainer.appendChild(div);
     })
+}
 
+
+window.addEventListener("load", async ()=>{
+   showPics();
 })
 
-mainContainer.addEventListener("mousemove",(m)=>{
-    let cRect = cursor.getBoundingClientRect();
-    gsap.to(cursor, {
-        x:m.clientX - cRect.width/2,
-        y:m.clientY - cRect.height/2
-    })
-})
-
-window.addEventListener("scroll", async function() {
-    if (Math.ceil(window.scrollY + window.innerHeight) >= document.body.offsetHeight) {
-        let fetfotu = await fetch('https://api.unsplash.com/photos/random/?client_id=nOZ8-oQTkpz_oTsDbhOoNSsuzj0drJWYzfM2Ej20DfU&count=10');
-        let jfotu = await fetfotu.json();
-        jfotu.forEach(fotus =>{
-            let div = document.createElement("div");
-            let code = `
-                <div class="card">
-                    <img src="${fotus["urls"]["regular"]}" alt="">
-                </div>
-            `
-            div.innerHTML = code;
-            fotuContainer.appendChild(div);
-        })
+window.addEventListener("scroll", function() {
+    if (Math.ceil(window.scrollY + window.innerHeight) >= document.documentElement.scrollHeight) {
+        showPics();
     }
+    console.log("helo world");
 });
 
 
